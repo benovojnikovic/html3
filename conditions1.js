11322a8addecb7d05cc30c4a34664f200ac9aad6
benovojnikovic <vojnikbeno1@gmail.com>
2022-06-07 15:28:46 -0700

    this one

---
+++ conditions1.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+let randomNumber =Math.random(); //0.0-1
+
+// if (randomNumber < 0.5){
+
+// console.log("condition was met");
+// console.log(randomNumber);
+
+// }
+
+
+if (randomNumber >=0.5){
+
+console.log('condition was met');
+console.log(randomNumber);
+}
